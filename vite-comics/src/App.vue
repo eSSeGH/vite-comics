4621220d1fa13c5705342854670953fa3937bca6
eSSeGH <ste.virgadaula@gmail.com>
2023-03-01 16:38:30 +0100

    TICKET header suspended

---
+++ vite-comics/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import "@fontsource/roboto-condensed"
+
+export default {
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style scoped></style>
---
+++ vite-comics/src/components/AppHeader.vue
@@ -0,0 +1,94 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header class="main-header">
+
+        <div class="container flex-center">
+
+            <div class="logo">
+                <img src="/img/dc-logo.png" alt="dc logo horizontal">
+            </div>
+
+            <nav class="navbar">
+
+                <ul style='display: contents'>
+                    <li class="nav-link">
+                        <a href="">CHARACTERS</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">COMICS</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">MOVIES</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">TV</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">GAMES</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">COLLECTIBLES</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">VIDEOS</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">FANS</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">NEWS</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">SHOP</a>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/general.scss' as *;
+@use '../style/partials/colors';
+
+.main-header {
+    background-color: white;
+    height: 120px;
+
+    .container {
+        height: 100%;
+        margin: 0 auto;
+        justify-content: space-between;
+
+        .logo {
+            height: 82px;
+            aspect-ratio: 1;
+        }
+
+        .navbar {
+            height: 100%;
+            display: flex;
+            gap: 32px;
+
+            .nav-link {
+                height: 100%;
+                color: $black;
+                font-size: 0.75rem;
+                font-weight: 500;
+
+                &:active {
+                    border-bottom: 5px solid $blue;
+                }
+            }
+        }
+    }
+}
+</style>
