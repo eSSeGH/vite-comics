bbeea23c1b9f8845204147b7ef22b3869744e121
eSSeGH <ste.virgadaula@gmail.com>
2023-03-02 12:36:28 +0100

    FEAT AppLinks+AppLinksMenu added

--- src/components/AppLinks.vue
+++ src/components/AppLinks.vue
@@ -1,14 +1,105 @@
 <script>
+import AppLinksMenu from './AppLinksMenu.vue'
+
 export default {
+    components: {
+        AppLinksMenu,
+    },
+    data() {
+        return {}
+    }
 
 }
 </script>
 
 <template>
-    ciao
+    <section class="link-footer">
+
+        <div class="container">
+
+            <div class="container2">
+
+                <div class="col">
+
+                    <AppLinksMenu :title="'DC COMICS'" :footerLinks="[
+                        'Characters',
+                        'Comics',
+                        'Movies',
+                        'TV',
+                        'Games',
+                        'Videos',
+                        'News',
+                    ]" />
+
+                    <AppLinksMenu :title="'SHOP'" :footerLinks="[
+                        'Shop DC',
+                        'Shop DC Collectibles',
+                    ]" />
+                </div>
+
+                <div class="col">
+
+                    <AppLinksMenu :title="'DC'" :footerLinks="[
+                        'Terms of use',
+                        'Privacy policy (New)',
+                        'Ad Choices',
+                        'Advertising',
+                        'Jobs',
+                        'Subscriptions',
+                        'Talent Workshops',
+                        'CPSC Certificates',
+                        'Ratings',
+                        'Shop Help',
+                        'Contact Us',
+                    ]" />
+                </div>
+
+                <div class="col">
+
+                    <AppLinksMenu :title="'SITES'" :footerLinks="[
+                        'DC',
+                        'MAD Magazine',
+                        'DC Kids',
+                        'DC Universe',
+                        'DC Power Visa',
+                    ]" />
+                </div>
+
+            </div>
+
+        </div>
+
+        <img src="/img/dc-logo-bg.png" alt="">
+
+    </section>
 </template>
 
 <style lang="scss" scoped>
 @use '../style/general.scss' as *;
-@use '../style/partials/colors';
+@use '../style/partials/colors' as *;
+
+.link-footer {
+    background-image: url(/img/footer-bg.jpg);
+    position: relative;
+    overflow-y: hidden;
+    padding-bottom: 30px;
+
+    .container2 {
+        width: 30%;
+        display: flex;
+        gap: 28px;
+
+        .col {
+            flex-basis: calc(100% - 28px*2/3);
+        }
+    }
+
+    img {
+        height: 563px;
+        aspect-ratio: 1;
+        position: absolute;
+        top: -70px;
+        right: 320px;
+    }
+}
 </style>
---
+++ src/components/AppLinksMenu.vue
@@ -0,0 +1,88 @@
+<script>
+export default {
+    data() {
+        return {
+            title: '',
+            footerLinks: [],
+        }
+    },
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        footerLinks: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h3>{{ title }}</h3>
+
+        <ul>
+            <li v-for="(link, i) in footerLinks" :key="i">
+                <a href="">{{ link }}</a>
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/general.scss' as *;
+@use '../style/partials/colors' as *;
+
+.link-footer {
+    background-image: url(/img/footer-bg.jpg);
+    height: 372px;
+    position: relative;
+    overflow-y: hidden;
+
+    .container2 {
+        width: 30%;
+        display: flex;
+        gap: 28px;
+
+        .col {
+            flex-basis: calc(100% - 28px*2/3);
+
+            h3 {
+                color: white;
+                font-size: 1.2rem;
+                padding: 20px 0;
+            }
+
+            ul {
+                display: flex;
+                flex-direction: column;
+                gap: 5px;
+
+                li {
+
+                    a {
+                        color: lightgrey;
+                        font-size: 0.75rem;
+
+                        &:hover {
+                            color: white;
+                            text-decoration: underline;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    img {
+        height: 563px;
+        aspect-ratio: 1;
+        position: absolute;
+        top: -70px;
+        right: 320px;
+    }
+}
+</style>
